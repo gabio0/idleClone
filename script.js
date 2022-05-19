72b1ddbc5673aff4d5c0536bf8617457eb03ec63
gabio0 <gabriel.oakey@gmail.com>
2022-05-18 22:20:43 -0600

    Added 2 menus and more functionality

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const progressPixelWidth = 300
-
 /*question elements store the text for the job-title inside a span, and they
 directly store the math question*/
 const question = document.querySelectorAll(".question")
@@ -8,7 +6,8 @@ const progress = document.querySelectorAll(".progress")
 const input = document.querySelector(".input")
 const money = document.querySelector(".money")
 const questionsAnswered = document.querySelector(".questions-answered")
-
+const displayMoneySeconds = document.querySelector(".money-seconds")
+const displayStatus = document.querySelector(".status")
 /*progress elements get a bool to check if it's loading (can't answer problems
 if it's still loading) and a loadingTime variable to tell it how long to load*/
 progress.forEach((e) => e.isProgressRunning = false)
@@ -18,11 +17,21 @@ const operatorList = ['+','-','+','-','*','/','+','-','*','/']
 const loadingTime = [17, 29, 49, 84, 142, 241, 410, 698, 1186, 2016]
 const operandMax = [5,5,25,25,5,5,100,100,10,10]
 const payday = [1,2,5,10,20,50,100,250,500,1000]
-const job = ['Burger-Flipper','Waitress','Garbageman','Teacher','Firefighter',
-'Lawyer','Doctor','Programmer','Politician','Oligarch']
-let selector = ['A','S','D','F','G','Z','X','C','V','B']
+const job = ['Troll Poop cleaner','Narwhal Breeder','Dragon Groomer','Magical Zoologist','Hydra Slayer', 'Wildcat Trainer','Alchemist','Wizard','White Mage','Unicorn Rider']
+const selector = ['A','S','D','F','G','Z','X','C','V','B']
+const peonBaseCosts = [10,50,200,1000,3500,10000,50000,300000,2000000,10000000]
+const peonNames = ['Hobo Temp A','Little Girl S','Fan Boy D','Scientist F','Knight G','Tank Z','Insane Old Guy X','Creepy Hag C','Best Friend V','Master B']
+const statusItem = ['Hair Cut A', 'New Shoes S', 'Hire Maid F', 'New Car D', 'Fly First Class G', 'Hire Chocolatier Z', 'Live-in Nanny X', 'Beach House C', 'Name a Library V', 'Tophat & Monocle B']
+const statusCost = [30,150,500,2000,7500,18000,50000,500000,3000000,10000000]
+const statusPointValue = [1,4,9,16,25,37,50,65,82,99]
+let inHireMenu = false
+let inStatusMenu = false
+let inResetMenu = false
+let moneyPerSecond = 0
+let statusPoints = 0
+let isMoneyPerSecondRunning = false;
 
-
+//loading additional element attributes
 for(let i=0;i<progress.length;i++) {
     progress[i].loadingTime = loadingTime[i]
 }
@@ -33,6 +42,12 @@ for(let i=0;i<question.length;i++) {
     question[i].selector = selector[i]
     question[i].payday = payday[i]
     question[i].job = job[i]
+    question[i].countHired = 0;
+    question[i].peonCost = peonBaseCosts[i]
+    question[i].peonName = peonNames[i]
+    question[i].statusItem = statusItem[i]
+    question[i].statusCost = statusCost[i]
+    question[i].statusPointValue = statusPointValue[i]
     question[i].children[1].textContent += generateQuestion(question[i])
 }
 
@@ -65,29 +80,28 @@ function generateQuestion(element) {
     return `${a} ${element.operator} ${b} = ${element.selector}`
 }
 
+//checking answers relies on the string-math questions following this specific format # <operator> #
 function checkAnswers(element) {
     const a = element.children[1].textContent.split(' ', 3)
     switch (a[1]) {
-        case('+') : return parseInt(a[0]) + parseInt(a[2])
-        case('-') : return parseInt(a[0]) - parseInt(a[2])
-        case('*') : return parseInt(a[0]) * parseInt(a[2])
-        case('/') : return parseInt(a[0]) / parseInt(a[2])
+        case('+') : return parseFloat(a[0]) + parseFloat(a[2])
+        case('-') : return parseFloat(a[0]) - parseFloat(a[2])
+        case('*') : return parseFloat(a[0]) * parseFloat(a[2])
+        case('/') : return parseFloat(a[0]) / parseFloat(a[2])
     }
 }
 
 //if you get the selected question right
 function updateScore(element) {
     const tempArray = questionsAnswered.textContent.split(" ")
-    let tempArray2 = money.textContent.split(" ")
-    tempArray2[1] = tempArray2[1].substring(1)
-    a = parseInt(tempArray[2]) + 1
-    b = parseInt(tempArray2[1]) + parseInt(element.payday)
+    a = parseFloat(tempArray[2]) + 1
+    b = getDisplayMoney() + parseFloat(element.payday)
     questionsAnswered.textContent = 'Questions Answered: ' + a
-    money.textContent = "Money: $" + b 
+    setDisplayMoney(b)
 }
 
 //when you press one of the letter-selectors
-function selectorPress(entry) {
+function questionSelected(entry) {
     entry = entry.toLowerCase();
     const selectedQuestion = document.getElementById(entry)
     const selectedProgress = document.getElementById(entry + "Progress")
@@ -101,12 +115,115 @@ function selectorPress(entry) {
     return;
 }
 
-window.addEventListener('keydown', (e) => {
-    if(e.key >= 0 && e.key <=9 && input.textContent.length < 8) {
-        if(input.textContent == '0') input.textContent = e.key;
-        else input.textContent += e.key
+function backToMainScreen() {
+    input.textContent = '0'
+    input.style = 'font-size: 60px;'
+    for(let i=0;i<question.length;i++) {
+        question[i].children[0].textContent = `$${question[i].payday} ${question[i].job}`
+        question[i].children[1].textContent = generateQuestion(question[i])
     }
-    if("asdfgzxcvbASDFGZXCVB".includes(e.key)) selectorPress(e.key)
+}
+
+function openHireMenu() {
+    inHireMenu = !inHireMenu;
+    if(inHireMenu){
+        input.style = 'font-size: 31px;'
+        input.textContent = 'Hire Some Peons!'
+        for(let i=0;i<question.length;i++) {
+            question[i].children[0].textContent = `Cost $${question[i].peonCost} to hire:`
+            question[i].children[1].textContent = `${peonNames[i]}`
+        }
+    } else backToMainScreen()
+}
+
+function openStatusMenu() {
+    inStatusMenu = !inStatusMenu
+    if(inStatusMenu) {
+        input.style = 'font-size: 31px;'
+        input.textContent = 'Be Cooler!'
+        for(let i=0;i<question.length;i++) {
+            question[i].children[0].textContent = `Cost $${question[i].statusCost} for a`
+            question[i].children[1].textContent = question[i].statusItem
+        }
+    } else backToMainScreen()
+}
+
+function updateMoneyPerSecond(){
+    if(isMoneyPerSecondRunning) clearInterval(seconds)
+    let a = 0;
+    for(let i=0;i<question.length;i++){
+        a += question[i].countHired / (progress[i].loadingTime * .3)
+    }
+    moneyPerSecond = a.toFixed(2)
+    seconds = setInterval(moneySeconds, 1000)
+    function moneySeconds() {
+        isMoneyPerSecondRunning = true;
+        b = getDisplayMoney() + parseFloat(moneyPerSecond)
+        setDisplayMoney(b.toFixed(2))
+    }
+    displayMoneySeconds.textContent = '$' + moneyPerSecond + '/second'
+}
+
+function getDisplayMoney() {
+    let a = money.textContent.split('$')
+    return parseFloat(a[1])
+}
+
+function setDisplayMoney(m) {
+    money.textContent = "Money: $" + m
+}
+
+function adjustDisplayMoney(a) {
+    const b = getDisplayMoney()
+    const c = (b - a).toFixed(2)
+    money.textContent = "Money: $" + c
+    return
+}
+
+function canIAfford(a) {
+    const b = getDisplayMoney()
+    if(b >= a) {
+        return true
+    } else return false
+}
+
+function someoneHired(entry) {
+    const selectedQuestion = document.getElementById(entry)
+    if(canIAfford(selectedQuestion.peonCost)){
+        adjustDisplayMoney(selectedQuestion.peonCost)
+        selectedQuestion.countHired++
+        selectedQuestion.peonCost = (selectedQuestion.peonCost * 1.5).toFixed(2)
+        selectedQuestion.children[0].textContent = `Cost $${selectedQuestion.peonCost} to hire:`
+        updateMoneyPerSecond()
+    }
+    return;
+}
+
+function statusPurchase(entry) {
+    const selectedQuestion = document.getElementById(entry)
+    if(canIAfford(selectedQuestion.statusCost)){
+        adjustDisplayMoney(selectedQuestion.statusCost)
+        selectedQuestion.statusCost = (selectedQuestion.statusCost * 1.5).toFixed(2)
+        statusPoints += selectedQuestion.statusPointValue
+        displayStatus.textContent = `You are a: POOR with ${statusPoints} status`
+        updateMoneyPerSecond()
+    }
+}
+
+window.addEventListener('keydown', (e) => {
+    const a = e.key.toLowerCase();
+    if(a >= 0 && a <=9 && input.textContent.length < 8) {
+        if(input.textContent == '0') input.textContent = a;
+        else input.textContent += a
+    }
+    if("asdfgzxcvbASDFGZXCVB".includes(a)) {
+        if(inStatusMenu) statusPurchase(a)
+        else if(inHireMenu) someoneHired(a)
+        else if(inResetMenu) resetPurchase(a)
+        else questionSelected(a)
+    }
+    if(a === 'w') openHireMenu()
+    if(a === 'q') openStatusMenu()
 })
 
-question.forEach(e => e.onclick = function() {selectorPress(e.selector)})
+question.forEach(e => e.onclick = function() {questionSelected(e.selector)})
